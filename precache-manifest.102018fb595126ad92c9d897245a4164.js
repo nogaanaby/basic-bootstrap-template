d08609eda7b09f12a2e2c9470bc8f1351d3beb0f
Noga Anaby <nogaanaby@gmail.com>
2018-10-05 15:47:55 +0300

    Updates

---
+++ precache-manifest.102018fb595126ad92c9d897245a4164.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/olamay-hen2/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "27478d149434af578235",
+    "url": "/olamay-hen2/static/js/runtime~main.27478d14.js"
+  },
+  {
+    "revision": "a9fb97c63d25adb1daad",
+    "url": "/olamay-hen2/static/js/main.a9fb97c6.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/olamay-hen2/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "a9fb97c63d25adb1daad",
+    "url": "/olamay-hen2/static/css/main.1d23d9a1.chunk.css"
+  },
+  {
+    "revision": "ea367a494d878ffa40f2ad85ae446598",
+    "url": "/olamay-hen2/index.html"
+  }
+];
