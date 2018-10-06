9282762ce90445bde18d77d02f439f7e054f1f1b
Noga Anaby <nogaanaby@gmail.com>
2018-10-06 23:09:18 +0300

    vhgbgk

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -6,9 +6,24 @@ class About extends Component {
   render() { 
     return ( 
       <div className="about">
-      <img className="night" src={night} alt=""/>
-    
-      </div> 
+        <img className="night" src={night} alt=""/>
+        <div className="details">
+          <div className="detail">
+            <h1>Managment</h1>
+            <h6>The Hole World Around Us</h6>
+            <small><b>Phone:</b> 123-456-7890
+              <br />
+              <b>Email:</b> info@mysite.com</small>
+          </div>
+          <div className="detail">
+            <h1>Architecture</h1>
+            <h6>The Hole World Above Us</h6>
+            <small><b>Phone:</b> 123-456-7890
+              <br />
+              <b>Email:</b> info@mysite.com</small>
+          </div>
+        </div>
+      </div>
     );
   }
 }
